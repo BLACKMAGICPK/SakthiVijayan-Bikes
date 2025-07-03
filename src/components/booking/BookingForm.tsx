f3c2dbedb6adf62a5504916f29ec9929388fe8d1
Prem Kumar <premkumarpk2724@gmail.com>
2025-07-03 07:40:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/booking/BookingForm.tsx
+++ src/components/booking/BookingForm.tsx
@@ -165,7 +165,7 @@ export default function BookingForm() {
                                     </FormControl>
                                 </PopoverTrigger>
                                 <PopoverContent className="w-auto p-0" align="start">
-                                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < (form.getValues("pickupDate") || new Date()))} initialFocus />
+                                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < (form.getValues("pickupDate") || new Date())} initialFocus />
                                 </PopoverContent>
                             </Popover>
                             <FormMessage />
